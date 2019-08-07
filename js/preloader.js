bbbe667e4ea0eab2e4694f1455885d431b5240e5
Erica <ericaremmele@gmail.com>
2019-08-06 21:29:03 -0400

    working to crack the loader

--- js/preloader.js
+++ js/preloader.js
@@ -1,5 +1,5 @@
 $(window).on('load', function() { // makes sure the whole site is loaded
   $('#status').fadeOut(); // will first fade out the loading animation
-  $('#preloader').delay(300).fadeOut('slow'); // will fade out the white DIV that covers the website.
-  $('body').delay(300).css({'overflow':'visible'});
+  $('#preloader').delay(5000).fadeOut('slow'); // will fade out the white DIV that covers the website.
+  $('body').delay(600).css({'overflow':'visible'});
 })
