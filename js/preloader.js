9cec670f535d3e1c2d6e17375e28fd492c737146
Erica <ericaremmele@gmail.com>
2019-08-10 13:11:53 -0600

    removing glitches, finessing text

--- js/preloader.js
+++ js/preloader.js
@@ -1,5 +1,5 @@
 $(window).on('load', function() { // makes sure the whole site is loaded
   $('#status').fadeOut(); // will first fade out the loading animation
-  $('#preloader').delay(5000).fadeOut('slow'); // will fade out the white DIV that covers the website.
+  $('#preloader').delay(1000).fadeOut('slow'); // will fade out the white DIV that covers the website.
   $('body').delay(600).css({'overflow':'visible'});
 })
